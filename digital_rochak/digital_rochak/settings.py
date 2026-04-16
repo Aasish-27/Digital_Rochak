0ee7698245d6fd50b823aa40843a982ef8c4267a
Aasish <aasishkarki27@gmail.com>
2026-04-16 13:14:13 +0545

    Deployment Ready Update

--- digital_rochak/digital_rochak/settings.py
+++ digital_rochak/digital_rochak/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s2*qy6_m0f1xnoqwhy!h7_z=j#0i$@5%4iwf6b#-i)^d2t@cik'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = [".onrender.com"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
