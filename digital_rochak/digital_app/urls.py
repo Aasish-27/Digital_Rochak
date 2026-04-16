ce588e339424ed394d04815e11bf02c97bd18503
Aasish <aasishkarki27@gmail.com>
2026-04-16 11:29:18 +0545

    Adding about page and updated header

--- digital_rochak/digital_app/urls.py
+++ digital_rochak/digital_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views.main_views import home_view
+from .views.main_views import home_view,about_view,services_view,contact_view
 
 urlpatterns = [
     path('',home_view,name='home'),
+    path('about/',about_view,name='about'),
+    path('services/',services_view,name='services'),
+    path('contact/',contact_view,name='contact'),
 ]
--- digital_rochak/digital_app/views/main_views.py
+++ digital_rochak/digital_app/views/main_views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_view(request):
-    return render(request,'index.html')
+    return render(request,'main/index.html')
+
+def about_view(request):
+    return render(request,'main/about.html')
+
+def services_view(request):
+    return render(request,'main/services.html')
+
+def contact_view(request):
+    return render(request,'main/contact.html')
